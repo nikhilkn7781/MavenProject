6384e72d72f05da0f70a42ceb486865b39fd31f3
nikhil <nikhilkn7781@gmail.com>
2022-05-08 20:01:14 +0530

    Initial commit

---
+++ src/test/java/MavenJavaClass.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+public class MavenJavaClass {
+
+    @Test
+    public void test01(){
+        System.out.println("Inside test01");
+    }
+    @Test
+    public void test02(){
+        System.out.println("Inside test02");
+    }
+}
---
+++ src/test/java/NoOfOccurence.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class NoOfOccurence {
+    public static void main(String[] args) {
+        String s="chethan";
+        char[] t = s.toCharArray();
+        Map <Character, Integer> map=new HashMap<>();
+
+        for (char c:t) {
+            if (map.containsKey(c)) {
+                map.put(c, map.get(c)+1);
+            }else {
+                map.put(c, 1);
+            }
+        }
+        System.out.println(map);
+
+        Set<Map.Entry<Character, Integer>> entry= map.entrySet();
+        for (Map.Entry<Character, Integer> entrySet:entry) {
+            if (entrySet.getValue()>1) {
+                System.out.print(entrySet+" ");
+            }
+        }
+    }
+}
